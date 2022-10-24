de4bde008cc48e3c6280ce4dfde3b543f0c7e933
Arthur Ezeagbo <arthur.afamuefuna@ttpsc.pl>
2022-10-24 09:15:39 +0100

    MAJOR: Updated bean configuration

--- java-monitor-service/src/main/java/com/arthurezeagbo/javamonitorservice/config/AppBeanConfig.java
+++ java-monitor-service/src/main/java/com/arthurezeagbo/javamonitorservice/config/AppBeanConfig.java
@@ -1,8 +1,12 @@
 package com.arthurezeagbo.javamonitorservice.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
 import org.modelmapper.Conditions;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
@@ -10,6 +14,7 @@ import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilde
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
 import java.time.format.DateTimeFormatter;
 
 @Configuration
@@ -37,6 +42,13 @@ public class AppBeanConfig {
 
     @Bean
     public ObjectMapper objectMapper(){
-        return new ObjectMapper();
+        return JsonMapper.builder()
+                .findAndAddModules()
+                .build();
+    }
+
+    @Bean
+    public LockProvider lockProvider(DataSource dataSource) {
+        return new JdbcTemplateLockProvider(dataSource);
     }
 }
