76d670250e6e42306211e9dac597b59c964c9542
Arthur Ezeagbo <arthur.afamuefuna@ttpsc.pl>
2022-10-24 09:16:57 +0100

    MAJOR: Enabled scheduling

--- java-monitor-service/src/main/java/com/arthurezeagbo/javamonitorservice/JavaMonitorServiceApplication.java
+++ java-monitor-service/src/main/java/com/arthurezeagbo/javamonitorservice/JavaMonitorServiceApplication.java
@@ -1,8 +1,12 @@
 package com.arthurezeagbo.javamonitorservice;
 
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "PT10S")
 @SpringBootApplication
 public class JavaMonitorServiceApplication {
 
@@ -10,6 +14,5 @@ public class JavaMonitorServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JavaMonitorServiceApplication.class, args);
 	}
-
 
 }
